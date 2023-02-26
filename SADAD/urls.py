c07928d7d9a3b7b318cf89c72703259a005a1147
mohammed0115 <mohemmedkamal0115498898@gmail.com>
2023-02-26 13:19:09 +0200

    test

--- SADAD/urls.py
+++ SADAD/urls.py
@@ -59,8 +59,4 @@ urlpatterns = [
         
 
     
-]
-
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-               static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
