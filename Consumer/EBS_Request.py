a4dd1b1d1b5695f2a230731b37f74c78b768c07c
mohammed0115 <mohemmedkamal011598898@gmail.com>
2023-03-13 09:45:08 +0200

    serializer error

--- Consumer/EBS_Request.py
+++ Consumer/EBS_Request.py
@@ -25,8 +25,8 @@ class BaseEBSAPIView(views.APIView):
     verify_ssl = True
     validated_data = None
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer_class(data=request.data)
-        # serializer.is_valid(raise_exception=True)
+        # serializer = self.get_serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
         if serializer.is_valid():
             payload = self.get_payload_from_input(serializer.data)
             self.validated_data = serializer.validated_data
