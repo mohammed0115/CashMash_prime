b532f0beae56068fff36db9a3e3a1eeb80b0b60d
mohammed0115 <mohemmedkamal011598898@gmail.com>
2023-03-19 09:27:27 +0200

    add web portal

--- registerCard/urls.py
+++ registerCard/urls.py
@@ -1,4 +1,9 @@
-from registerCard.views import RegisterGolenCard,RegisterAgentCard,registerSilverCard,VirtualCard,RegisterList
+# from registerCard.views import RegisterGolenCard,RegisterAgentCard,registerSilverCard,VirtualCard,RegisterList
+from registerCard.views import registerSilverCard
+from registerCard.views import RegisterGolenCard
+from registerCard.views import RegisterAgentCard
+from registerCard.views import VirtualCard
+from registerCard.views import RegisterList
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import path,include
 
