b8cd5735440dc2c22797357d0e49135ffb500525
mohammed0115 <mohemmedkamal011598898@gmail.com>
2023-03-21 17:20:14 +0200

    remove unwanted folder

--- Consumer/serializers.py
+++ Consumer/serializers.py
@@ -250,8 +250,8 @@ class BillInquiryConsumerAPISerializer(
     
     """
     # no extra fields, just the combined CardRequiredConsumerAPISerializer and PaymentInfoConsumerAPISerializer fields
-    PAN = serializers.CharField(allow_null=False, validators=[PanValidator()])
+    # PAN = serializers.CharField(allow_null=False, validators=[PanValidator()])
     # IPIN = serializers.CharField(max_length=88, required=False,allow_null=True)
-    expDate = serializers.DateField(format='%y%m', input_formats=['%y%m'], allow_null=True)
+    # expDate = serializers.DateField(format='%y%m', input_formats=['%y%m'], allow_null=True)
     mbr = serializers.CharField(max_length=3, min_length=1, required=False,allow_null=True)  
 
