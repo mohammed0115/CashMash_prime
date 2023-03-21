097bd44b1bfa5f170afb767e3f361b7a69c7b00a
mohammed0115 <mohemmedkamal011598898@gmail.com>
2023-03-21 14:23:50 +0200

    change base url and other ebs gatwat

--- registerCard/serializers.py
+++ registerCard/serializers.py
@@ -98,15 +98,17 @@ class GoldenCardSerializer(RegisterSerializer):
 #         fields = ('transaction_id', 'tranDateTime', 'last_4_digits_of_pan',
 #                   'tranAmount', 'tranCurrency', 'response_message',
 #                   'response_status', 'transaction_fee', 'additional_amount')    
-class PhysicalCardSerializer(serializers.ModelSerializer):
-    tranDateTime = serializers.RegexField('^[0-9]{12}',max_length=12,allow_null=False)
-    UUID = serializers.RegexField('^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}',max_length=36, min_length=36, allow_blank=False)
-    # registrationType      = serializers.CharField(validators=[registrationTypeValidator()],max_length=2,allow_null=False)
-    # # financialInstitutionId=serializers.CharField(max_length=4, required=False,allow_null=True)
-    # panCategory           =serializers.CharField(max_length=10, required=False,allow_null=True)
-    # # job                   =serializers.CharField(max_length=50, required=False,allow_null=True)
-    # # email                 =serializers.EmailField( required=False,allow_null=True)
-    # # extraInfo             =extraInforSerializer(required=False)
+class PhysicalCardSerializer(EntityUserAPISerializer,UserNameSerializer,
+                         userPasswordserializer,BaseConsumerAPISerializer,
+                         CardRequiredInfoAPISerializer,
+                         PanSerializer
+                         ):
+    registrationType      = serializers.CharField(max_length=2,allow_null=False)
+    # financialInstitutionId=serializers.CharField(max_length=4, required=False,allow_null=True)
+    panCategory           =serializers.CharField(max_length=10, required=False,allow_null=True)
+    # job                   =serializers.CharField(max_length=50, required=False,allow_null=True)
+    # email                 =serializers.EmailField( required=False,allow_null=True)
+    # extraInfo             =extraInforSerializer(required=False)
     """
 {
        "applicationId":"ITQAN",
@@ -127,27 +129,26 @@ class PhysicalCardSerializer(serializers.ModelSerializer):
         "panCategory": "Standard"
         }
 """
-    class Meta:
-        model=Register
-        fields=[
-                'tranDateTime',
-                'UUID',
-                'userName',
-                'entityId',
-                'entityType',
-                'entityGroup',
-                'phoneNo',
-                'registrationType',
-                'userPassword',
-                # 'financialInstitutionId',
-                'panCategory',
-                # 'job',
-                # 'email',
-                'PAN',
-                'IPIN',
-                'expDate',
-                # 'mbr'
-                ]
+    # class Meta:
+    #     model=Register
+    #     fields=['tranDateTime',
+    #             'UUID',
+    #             'userName',
+    #             'entityId',
+    #             'entityType',
+    #             'entityGroup',
+    #             'phoneNo',
+    #             'registrationType',
+    #             'userPassword',
+    #             # 'financialInstitutionId',
+    #             'panCategory',
+    #             # 'job',
+    #             # 'email',
+    #             'PAN',
+    #             'IPIN',
+    #             'expDate',
+    #             'mbr'
+    #             ]
     # def get_validation_exclusions(self):
         # exclusions = super(PhysicalCardSerializer, self).get_validation_exclusions()
         # return exclusions + ['mbr']
