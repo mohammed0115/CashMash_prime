fea86c772f6a543dc5cb91f3714bc91f3d3237e1
mohammed0115 <mohemmedkamal011598898@gmail.com>
2023-03-23 10:32:29 +0200

    change base url and other ebs gatwat

--- Consumer/views.py
+++ Consumer/views.py
@@ -715,6 +715,7 @@ class RequestPinChangeView(EBSRequestAPIView):
             payload = self.get_payload_from_input(serializer.data)
             self.validated_data = serializer.validated_data
             try:
+                payload.update({'applicationId': 'ITQAN'})
                 ebs_response = self.ebs_post(payload)
             except requests.exceptions.ConnectionError:
                 # logger = self.get_logger()
